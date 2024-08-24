3cea1dcf3d7f65570f5fe1c2f662f14b4dd1d68a
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-08-24 07:47:44 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Give the class a name
+class PersonalAssistant:
+
+  
+  # Add an __init__ function here
+  def __init__(self):
+    self.contacts = {'Ann': 'Marketing Coordinator', 'Chelsea': 'Software Developer', 'Nichole': 'Sales Representative', 'Max': 'Technical Writer'}
+    self.todos = []
+
+  # Complete the get_contact function code
+  def get_contact(self, name):
+    if name in self.contacts:
+      return self.contacts[name]
+    else:
+      return "There's no contact by that name"
+
+
+# Code to test output of the class
+assistant = PersonalAssistant()
+# Change name to one from your contacts
+print(assistant.get_contact("Chelsea"))
